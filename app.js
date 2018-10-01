97ada6bd19ce3d79285891bdef0ae5b328b2bf35
Catherine Looper <loopercat@gmail.com>
2018-10-01 14:06:39 -0700

    added linter, gitignore, and functionality for moving on x axis

--- app.js
+++ app.js
@@ -2,6 +2,19 @@
 
 const CANVAS_BORDER_COLOR = 'black';
 const CANVAS_BACKGROUND_COLOR = 'salmon';
+const SNAKE_COLOR = 'forestGreen';
+const SNAKE_BORDER_COLOR = 'darkGreen';
+
+let snake = [
+  {x: 150, y: 150},
+  {x: 140, y: 150},
+  {x: 130, y: 150},
+  {x: 120, y: 150},
+  {x: 110, y: 150},
+];
+
+let dx = 10;
+let dy = 0;
 
 let game = document.getElementById('game');
 
@@ -12,17 +25,13 @@ ctx.strokestyle = CANVAS_BORDER_COLOR;
 ctx.fillRect(0, 0, game.width, game.height);
 ctx.strokeRect(0, 0, game.width, game.height);
 
-let snake = [
-  {x: 150, y: 150},
-  {x: 140, y: 150},
-  {x: 130, y: 150},
-  {x: 120, y: 150},
-  {x: 110, y: 150},
-];
+moveSnake();
+
+drawSnake();
 
 function drawSnakePiece(snek) {
-  ctx.fillStyle = 'forestGreen';
-  ctx.strokestyle = 'darkGreen';
+  ctx.fillStyle = SNAKE_COLOR;
+  ctx.strokestyle = SNAKE_BORDER_COLOR;
 
   ctx.fillRect(snek.x, snek.y, 10, 10);
   ctx.strokeRect(snek.x, snek.y, 10, 10);
@@ -32,4 +41,11 @@ function drawSnake() {
   snake.forEach(drawSnakePiece);
 }
 
-drawSnake();
+function moveSnake() {
+  const head = {x: snake[0].x + dx, y: snake[0].y};
+
+  snake.unshift(head);
+  snake.pop();
+}
+
+
