d8d9949d2f2c40fa7040c43000ea4fa2b098c42e
Catherine Looper <loopercat@gmail.com>
2018-10-02 14:34:34 -0700

    setting up local storage

--- js/draggable.js
+++ js/draggable.js
@@ -1,6 +1,49 @@
 'use strict';
 
-const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+Alphabet.all = [];
+
+function Alphabet(letter) {
+  this.letter = letter;
+  this.left = 0;
+  this.top = 0;
+  Alphabet.all.push(this);
+}
+
+let storedAlphabet = localStorage.getItem('alphabet');
+
+if(!storedAlphabet) {
+
+  new Alphabet('a');
+  new Alphabet('b');
+  new Alphabet('c');
+  new Alphabet('d');
+  new Alphabet('e');
+  new Alphabet('f');
+  new Alphabet('g');
+  new Alphabet('h');
+  new Alphabet('i');
+  new Alphabet('j');
+  new Alphabet('k');
+  new Alphabet('l');
+  new Alphabet('m');
+  new Alphabet('n');
+  new Alphabet('o');
+  new Alphabet('p');
+  new Alphabet('q');
+  new Alphabet('r');
+  new Alphabet('s');
+  new Alphabet('t');
+  new Alphabet('u');
+  new Alphabet('v');
+  new Alphabet('w');
+  new Alphabet('x');
+  new Alphabet('y');
+  new Alphabet('z');
+
+  localStorage.setItem('alphabet', JSON.stringify(Alphabet.all));
+} else {
+  Alphabet.all = JSON.parse(storedAlphabet);
+}
 
 const myFreezer = document.getElementById('freezer');
 const myDiv = document.getElementById('target');
@@ -21,13 +64,15 @@ function changeTheme(event) {
   }
 }
 
-for(let letter of alphabet) {
-  let magnet = document.createElement('p');
-  magnet.textContent = letter;
-  magnet.setAttribute('draggable', 'true');
-  magnet.setAttribute('id', letter);
-  magnet.addEventListener('dragstart', dragStartHandler);
-  myFreezer.appendChild(magnet);
+function createLetters() {
+  for(let i = 0; i < Alphabet.all.length; i++) {
+    let magnet = document.createElement('p');
+    magnet.textContent = Alphabet.all[i].letter;
+    magnet.setAttribute('draggable', 'true');
+    magnet.setAttribute('id', Alphabet.all[i].letter);
+    magnet.addEventListener('dragstart', dragStartHandler);
+    myFreezer.appendChild(magnet);
+  }
 }
 
 function dragStartHandler(event) {
@@ -51,8 +96,19 @@ function dropHandler(event) {
   elementSelected.style.zIndex = 1000;
   elementSelected.style.left = event.pageX - elementSelected.offsetWidth / 2 + 'px';
   elementSelected.style.top = event.pageY - elementSelected.offsetHeight / 2 + 'px';
+
+  for(let i = 0; i < Alphabet.all.length; i++) {
+    if(data === Alphabet.all[i].letter) {
+      Alphabet.all[i].left = elementSelected.style.left;
+      Alphabet.all[i].top = elementSelected.style.top;
+    }
+  }
+
+  localStorage.setItem('alphabet', JSON.stringify(Alphabet.all));
+
 }
 
+createLetters();
 
 myDiv.addEventListener('dragover', dragOverHandler);
 myDiv.addEventListener('drop', dropHandler);
